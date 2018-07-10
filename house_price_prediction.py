8989f19d0ff1ad37558d25a8c6834f2ddb814345
gaoyishu <gaoyishu91@163.com>
2018-07-11 00:17:19 +0800

    Create house_price_prediction.py

---
+++ house_price_prediction.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+%matplotlib inline 
+import matplotlib.pyplot as plt
+color = sns.color_palette()
+sns.set_style('darkgrid')
+import warnings
+def ignore_warnings(*args,**kwargs):
+    pass
+warnings.warn = ignore_warnings
+
+
+from scipy import stats
+from scipy.stats import norm,skew
+
+
+    
